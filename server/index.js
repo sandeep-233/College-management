6c7faa5c576ffa9c5bbcb1056e40d125749b15b5
sandeep-233 <sandeepkv233@gmail.com>
2024-07-04 12:24:27 +0530

    updated fontend url

--- server/index.js
+++ server/index.js
@@ -28,7 +28,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		// origin:"http://localhost:3000",
+		origin:"https://college-management-pi.vercel.app/",
 		credentials:true,
 	})
 )
