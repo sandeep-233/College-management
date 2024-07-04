1335a6e911d91bed95a3c2673a8723b6430c2b8f
sandeep-233 <sandeepkv233@gmail.com>
2024-07-04 12:33:57 +0530

    updated origin value in server index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(
 	cors({
 		// origin:"http://localhost:3000",
-		origin:"https://college-management-pi.vercel.app/",
+		origin:"https://college-management-pi.vercel.app",
 		credentials:true,
 	})
 )
